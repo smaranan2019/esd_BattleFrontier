928c9bdc91efd256faa98cab584b3b71e77f19ce
Rdice14 <56544853+Rdice14@users.noreply.github.com>
2021-03-17 23:57:42 +0800

    Added Pokemon and User services
    Used dummy sql table

---
+++ Pokemon.py
@@ -0,0 +1,99 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/esdProject'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+CORS(app)
+
+class PokemonCard(db.Model):
+    __tablename__ = 'PokemonCard'
+
+    card_id = db.Column(db.String(13), primary_key=True)
+    seller_id = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    level = db.Column(db.String(255))
+    image = db.Column(db.String(255))
+    price = db.Column(db.String(255))
+
+    def __init__(self, card_id, seller_id, name, level, image, price):
+        self.card_id = card_id
+        self.seller_id = seller_id
+        self.name = name
+        self.level = level
+        self.image = image
+        self.price = price
+
+    def json(self):
+        return {
+            "card_id": self.card_id,
+            "seller_id": self.seller_id,
+            "name": self.name,
+            "level": self.level,
+            "image": self.image,
+            "price": self.price
+        }
+
+@app.route('/addPokemonCard/<string:card_id>', methods=['POST'])
+def addPokemonCard(card_id):
+    try:
+        if (PokemonCard.query.filter_by(card_id=card_id).first()):
+            return jsonify(
+                {
+                    "code": 400,
+                    "data": {
+                        "card_id": card_id
+                    },
+                    "message": "Card already exists."
+                }
+            ), 400
+    except: pass
+
+    data = request.get_json()
+    pokemon_card = PokemonCard(card_id, **data)
+
+    try:
+        db.session.add(pokemon_card)
+        db.session.commit()
+    except Exception as e:
+        print(e)
+        return jsonify(
+            {
+                "code": 500,
+                "data": {
+                    "card_id": card_id
+                },
+                "message": "An error occurred creating the book."
+            }
+        ), 500
+
+    return jsonify(
+        {
+            "code": 201,
+            "data": pokemon_card.json()
+        }
+    ), 201
+
+@app.route("/findPokemonCard")
+def findPokemon():
+    data = request.get_json()
+    # DataModel:
+    # {
+    #     "name": name,
+    #     "level": level,
+    #     "price": price
+    # }
+    print(data)
+    card = PokemonCard.query.filter_by(name=data["name"], level=data["level"], price=data["price"])
+    if (card):
+        print(card)
+        return "Card found!"
+    else:
+        return "Card not found!"
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
---
+++ User.py
@@ -0,0 +1,102 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import random
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/esdProject'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+CORS(app)
+
+class User(db.Model):
+    __tablename__ = 'User'
+
+    user_id = db.Column(db.String(13), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    telegram_id = db.Column(db.String(255))
+    phone_number = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+
+    def __init__(self, user_id, name, email, telegram_id, phone_number, password):
+        self.user_id = user_id
+        self.name = name
+        self.email = email
+        self.telegram_id = telegram_id
+        self.phone_number = phone_number
+        self.password = password
+    
+    def json(self):
+        return{
+            "user_id": self.user_id,
+            "name": self.name,
+            "email": self.email,
+            "telegram_id": self.telegram_id,
+            "phone_number": self.phone_number,
+            "password": self.password
+        }
+
+@app.route('/addUser/<string:user_id>', methods=['POST'])
+def addUser(user_id):
+    data = request.get_json()
+    print(data)
+    try:
+        if (User.query.filter_by(user_id=user_id).first()):
+            return jsonify(
+                {
+                    "code": 400,
+                    "data": {
+                        "user_id": user_id
+                    },
+                    "message": "User already exists."
+                }
+            ), 400
+    except:
+        pass
+
+    user = User(user_id, **data)
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        print(e)
+        return jsonify(
+            {
+                "code": 500,
+                "data": {
+                    "user_id": user_id
+                },
+                "message": "An error occurred creating the user."
+            }
+        ), 500
+    
+    return jsonify(
+        {
+            "code": 201,
+            "data": user.json()
+        }
+    ), 201
+
+@app.route("/findUser/<string:email>")
+def findUser(email):
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return jsonify(
+            {
+                "code": 200,
+                "data": user.json()
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "User not found."
+        }
+    ), 404
+
+if __name__ == "__main__":
+    app.run(port=5000)
