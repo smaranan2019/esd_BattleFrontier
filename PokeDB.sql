cb592468c98152e269f6842deb08fd52be6a6e27
chelsealxy <73012678+chelsealxy@users.noreply.github.com>
2021-03-19 19:01:29 +0800

    Update PokeDB.sql

--- PokeDB.sql
+++ PokeDB.sql
@@ -1,3 +1,7 @@
+DROP DATABASE IF EXISTS PokeDB;
+CREATE DATABASE PokeDB;
+use PokeDB;
+
 create table account
 (User_ID varchar(255) not null primary key,
 FullName varchar(255) not null,
@@ -8,8 +12,10 @@ pswd varchar(255) not null
 );
 
 insert into account values
-('GottaCatchEmAll', 'Ash Ketchum', 'ilovepokemon@yahoo.com', '@pikapika', '12345678', 'IChooseYou123'),
-('ComeGeddit', 'Professor Willow', 'pickme@yahoo.com', '@pokeballer', '87654321', 'PickMe123');
+('GottaCatchEmAll', 'Ash Ketchum', 'ilovepokemon@yahoo.com', 
+'@pikapika', '12345678', 'IChooseYou123'),
+('ComeGeddit', 'Professor Willow', 'pickme@yahoo.com', 
+'@pokeballer', '87654321', 'PickMe123');
 
 create table card_list
 (Card_ID char(5) not null primary key,
@@ -18,7 +24,8 @@ NameOfCard varchar(255),
 LevelOfCard int,
 TypeOfCard varchar(10),
 Price decimal(5,2),
-constraint card_list_fk foreign key(User_ID) references account(User_ID)
+constraint card_list_fk foreign key(User_ID) references account
+(User_ID)
 );
 
 insert into card_list values
@@ -27,27 +34,72 @@ insert into card_list values
 ('C0003', 'ComeGeddit', 'Squirtle', '63', 'water', '1');
 
 create table orders
-(order_ID char(5) not null primary key AUTO_INCREMENT,
+(order_ID int not null primary key AUTO_INCREMENT,
 Card_ID char(5) not null,
 User_ID varchar(255) not null,
 status_appr varchar(255),
-constraint order_fk1 foreign key(Card_ID) references card_list(Card_ID),
-constraint order_fk2 foreign key(User_ID) references account(User_ID)
+constraint order_fk1 foreign key(Card_ID) references card_list
+(Card_ID),
+constraint order_fk2 foreign key(User_ID) references account
+(User_ID)
 );
 
 create table payment
-(Payment_ID char(5) not null primary key AUTO_INCREMENT, 
-order_ID char(5) not null,
+(Payment_ID int not null primary key AUTO_INCREMENT, 
+order_ID int not null,
 status_paid varchar(255),
-constraint payment_fk foreign key(order_ID) references order(order_ID)
+constraint payment_fk foreign key(order_ID) references orders
+(order_ID)
 );
 
 create table shipping
-(ShipID char(10) not null primary key AUTO_INCREMENT,
-Payment_ID char(5) not null,
+(ShipID int not null primary key AUTO_INCREMENT,
+Payment_ID int not null,
 status_send varchar(255),
 status_receive varchar(255),
-constraint shipping_fk foreign key(Payment_ID) references payment(Payment_ID)
+constraint shipping_fk foreign key(Payment_ID) references payment
+(Payment_ID)
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
