007bc51164473922bada8e53b8e55a1ddfde4b9e
Tananatham <73059344+Tananatham@users.noreply.github.com>
2021-03-20 16:42:17 +0800

    adding json data reader
    Code from sophia

--- TeleBot.py
+++ TeleBot.py
@@ -3,7 +3,8 @@ import time
 list = ['Hi there, welcome to pokemon battle frontier', 'Hot singles in your area want to meet you! :"))))', 'Alright wots all this then']
 
 token = '1738348275:AAHdhqyX3eXg3WskyUUnnxaOLOWvmNTlAEw'
-chat_id = '-514712203'
+chat_id = '78770764'
+base_URL = "https://api.telegram.org/bot"+token+"/sendMessage?chat_id="+chat_id+"&text='{}'".format(msg)
 
 for msg in list:
     base_URL = "https://api.telegram.org/bot"+token+"/sendMessage?chat_id="+chat_id+"&text='{}'".format(msg)
@@ -11,3 +12,12 @@ for msg in list:
     time.sleep(5)
 
 #https://api.telegram.org/bot1738348275:AAHdhqyX3eXg3WskyUUnnxaOLOWvmNTlAEw/sendMessage?chat_id=-514712203&text='Hello World'
+
+
+url = 'https://api.telegram.org/bot1738348275:AAHdhqyX3eXg3WskyUUnnxaOLOWvmNTlAEw/getUpdates'
+
+import urllib, json
+import urllib.request
+response = urllib.request.urlopen(url)
+data = json.loads(response.read())
+print(data)
