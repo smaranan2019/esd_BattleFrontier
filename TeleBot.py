03ddf575628cf68d3d3d6b82c28c4c10ab75701d
Tananatham <73059344+Tananatham@users.noreply.github.com>
2021-03-29 16:30:17 +0800

    Tantham
    sorry

--- TeleBot.py
+++ TeleBot.py
@@ -3,8 +3,8 @@ import time
 list = ['Hi there, welcome to pokemon battle frontier', 'Hot singles in your area want to meet you! :"))))', 'Alright wots all this then']
 
 token = '1738348275:AAHdhqyX3eXg3WskyUUnnxaOLOWvmNTlAEw'
-chat_id = '78770764'
-base_URL = "https://api.telegram.org/bot"+token+"/sendMessage?chat_id="+chat_id+"&text='{}'".format(msg)
+chat_id = '37579573'
+
 
 for msg in list:
     base_URL = "https://api.telegram.org/bot"+token+"/sendMessage?chat_id="+chat_id+"&text='{}'".format(msg)
@@ -14,10 +14,10 @@ for msg in list:
 #https://api.telegram.org/bot1738348275:AAHdhqyX3eXg3WskyUUnnxaOLOWvmNTlAEw/sendMessage?chat_id=-514712203&text='Hello World'
 
 
-url = 'https://api.telegram.org/bot1738348275:AAHdhqyX3eXg3WskyUUnnxaOLOWvmNTlAEw/getUpdates'
+# url = 'https://api.telegram.org/bot1738348275:AAHdhqyX3eXg3WskyUUnnxaOLOWvmNTlAEw/getUpdates'
 
-import urllib, json
-import urllib.request
-response = urllib.request.urlopen(url)
-data = json.loads(response.read())
-print(data)
+# import urllib, json
+# import urllib.request
+# response = urllib.request.urlopen(url)
+# data = json.loads(response.read())
+# print(data)
