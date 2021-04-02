2aa42b6bc6340da31448d67ec2a72de94ac4252a
Chi Tran <tranlinhchi2699@gmail.com>
2021-04-03 00:05:33 +0800

    OrderDB.sql can load import now

--- sql/Tabe DB.sql
+++
@@ -1,6 +0,0 @@
-create table payment
-(Payment_ID char(5) not null primary key AUTO_INCREMENT, 
-order_ID char(5) not null,
-status_paid varchar(255),
-constraint payment_fk foreign key(order_ID) references order(order_ID)
-);
